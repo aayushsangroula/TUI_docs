b3dd95006a9408169940ab6c2b0b33028507f228
Aayush Sangroula <aayush10sangroula@gmail.com>
2024-09-10 13:55:17 +1000

    Added add_bookmark function.

--- code_1.py
+++ code_1.py
@@ -1,3 +1,10 @@
 class Bookmarks:
     def __init__(self):
         self.store = {}
+
+    def add_bookmark():
+        print("You chose to add a new bookmark")
+        url = input("Enter bookmark URL: ")
+        new_bookmark = Bookmark(url)
+        bookmarks[len(bookmarks) + 1] = new_bookmark
+        print("Bookmark added successfully")
